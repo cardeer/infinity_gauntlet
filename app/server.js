27ce8da8eda5d58b58ff09de1d5087ae5844a636
CARDEER <cardeer123_1@hotmail.com>
2019-04-19 23:32:07 +0700

    no message

--- app/server.js
+++ app/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const port = 3000
 const app = express()
+const path = require('path')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../app/public/index.html"))
+})
 
 app.get('/api', (req, res) => {
     res.send('HELLO')
