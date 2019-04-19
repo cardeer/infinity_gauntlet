eed1e1f6b07cec9160e7cc1c395b1b54d11bb15e
CARDEER <cardeer123_1@hotmail.com>
2019-04-19 23:22:29 +0700

    minor update

--- app/server.js
+++ app/server.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const port = 3000
+const app = express()
+
+app.get('/api', (req, res) => {
+    res.send('HELLO')
+})
+
+app.listen(port, () => {
+    console.log('listening on port: ' + port)
+})
--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-    constructor(){
-        super()
+    onSelectProjectSubmit = e => {
+        e.preventDefault()
+        
     }
 
     render() {
         return (
             <div className="App">
-                
+                <form onSubmit={this.onSelectProjectSubmit}>
+                    <input type="text" placeholder="Project path URL" />
+                </form>
             </div>
         )
     }
