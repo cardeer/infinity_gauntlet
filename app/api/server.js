cd8eda752b5001d9c4b2ff94fa29daf9e2cc496a
cardeer <cardeer123_1@hotmail.com>
2019-04-20 14:10:20 +0700

    no message

--- app/api/server.js
+++ app/api/server.js
@@ -4,9 +4,33 @@ const app = express()
 
 const fs = require('fs')
 
-app.get('/path/:path', (req, res) => {
+function viewDirectory(path){
+    const projectPath = path
+    const files = fs.readdirSync(projectPath)
+    var allFiles = []
+    files.forEach(value => {
+        const currentPath = projectPath + '/' + value
+        const isFolder = fs.lstatSync(currentPath).isDirectory()
+        if (isFolder){
+            allFiles = allFiles.concat(viewDirectory(currentPath))
+        }
+        else allFiles.push([ value, currentPath, (fs.lstatSync(currentPath).size / (1024 * 1024)).toFixed(2) ])
+    })
+    return allFiles
+}
+
+app.get('/api/path/:path', (req, res) => {
     const projectPath = Buffer.from(req.params.path, 'base64').toString()
     const files = fs.readdirSync(projectPath)
+    var allFiles = []
+    // files.forEach(value => {
+    //     const currentPath = projectPath + '/' + value
+    //     const isFolder = fs.lstatSync(currentPath).isDirectory()
+    //     if (isFolder){
+    //         allFiles = allFiles.concat(viewDirectory(currentPath))
+    //     }
+    //     else allFiles.push([ value, currentPath, (fs.lstatSync(currentPath).size / (1024 * 1024)).toFixed(2) ])
+    // })
     res.send(files)
 })
 
--- app/src/App.js
+++ app/src/App.js
@@ -2,19 +2,45 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 class App extends Component {
+    state = {
+        files: []
+    }
     onSelectProjectSubmit = async e => {
         e.preventDefault()
-        const projectPath = e.target[0].value
-        const data = await axios.get(process.env.REACT_APP_API_URL + 'path/' + btoa(projectPath))
-        console.log(data)
+        const projectPath = btoa(e.target[0].value)
+        const files = await axios.get(process.env.REACT_APP_API_URL + `path/${projectPath}`)
+        this.setState({files: files.data})
     }
 
     render() {
         return (
             <div className="App">
-                <form onSubmit={this.onSelectProjectSubmit}>
-                    <input type="text" placeholder="Project path URL" name="project-dir" />
-                </form>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 col-md-6 offset-lg-2 offset-md-3">
+                            <div className="form-group text-center">
+                                <form onSubmit={this.onSelectProjectSubmit}>
+                                    <label>Project path</label>
+                                    <input type="text" className="form-control" placeholder="Project path URL" name="project-dir" autoComplete="false" />
+                                </form>
+                            </div>
+
+                            <div className="container-fluid">
+                                {
+                                    this.state.files && this.state.files.length > 0 &&
+                                    this.state.files.map((value, key) => {
+                                        // return <p key={key}>{value[0]} => {value[1]} ({value[2]}MB)</p>
+                                        return <p key={key}>{value}</p>
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <style>{`
+                
+                `}</style>
             </div>
         )
     }
